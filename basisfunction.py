0fb6ac906d599e6ac23807aa5f401c16d74db7f5
samuelmartins16 <36167144+samuelmartins16@users.noreply.github.com>
2018-09-18 20:36:41 -0300

    Teste 3 primeiras Basics Functions

--- basisfunction.py
+++ basisfunction.py
@@ -1,58 +1,56 @@
-'''CARACTERÍSTICAS DO ESTADO'''
+''' CARACTERISTICAS DO ESTADO '''
 
 
-#10 - QUANTIDADE DE PROJETOS DIVISÍVEIS
+# QUANTIDADE DE PROJETOS DIVISIVEIS
 
-def QprojDiv(estado_x):                                      #Função recebe os projetos como parâmetro do Estado
+def QprojDiv(estado_x):                                          #Funcao recebe os projetos como parametro do Estado
 
-    lpdiv = []                                              #Lista vazia que irá armazenar os projetos que são divisíveis
-    contpdiv = 0                                            #Contador que irá armazenar a quantidade de projetos divisíveis
-        for p in estado_x.P:                                #Para todos os projetos no conjunto de projetos 'P'
-            if( p.vdiv == 1):                               #Se, vdiv for igual a 1, significa que o projeto é divisível
-                lpdiv.append(p)                             #A lista recebe o projeto em questão
-                contpdiv = contpdiv + 1                     #Acréscimo de 1 no contador
+    lpdiv = []                                                   #Lista vazia que ira armazenar os projetos que sao divisiveis
+    contpdiv = 0                                                 #Contador que ira armazenar a quantidade de projetos divisiveis
+    for p in estado_x.P:                                         #Para todos os projetos no conjunto de projetos 'P'
+        if( p.vdiv == 1):                                        #Se, vdiv for igual a 1, significa que o projeto e divisivel
+            lpdiv.append(p)                                      #A lista recebe o projeto em questao
+            contpdiv = contpdiv + 1                              #Acrescimo de 1 no contador
 	
-    print('Projetos Divisíveis: \n')                        #Impressão dos projetos divisíveis
-        for p in self.lpdiv:
-            print p.nome
+    print('Projetos Divisiveis: \n')                             #Impressao dos projetos divisiveis
+    for p in self.lpdiv:
+        print p.nome
 
-    return contpdiv                                         #A função retorna o contador com a quantidade total de projetos em P capazes de serem divididos
-
-''' AS FUNÇÕES 11 E 12 SEGUEM A MESMA LÓGICA DA 10, APENAS MUDANDO O NOME DE ALGUNS PARÂMETROS/VARIÁVEIS LOCAIS '''
+    return contpdiv                                              #A funcao retorna o contador com a quantidade total de projetos em P capazes de serem divididos
 	
-#11 - QUANTIDADE DE PROJETOS DIVISÍVEIS EM CADA ÁREA
+#11 - QUANTIDADE DE PROJETOS DIVISIVEIS EM CADA AREA
 
 def QprojDiva(estado_x):
 
     lpdiva = []
     contpdiva = 0
-        for p in estado_x.P_a[a]:
-            if( p.vdiv == 1):
-                lpdiva.append(p)
-                contpdiva = contpdiva + 1
+    for p in estado_x.P_a[a]:
+        if( p.vdiv == 1):
+            lpdiva.append(p)
+            contpdiva = contpdiva + 1
 	
-    print('Projetos Divisíveis: \n')
-        for p in self.lpdiva:
-            print p.nome
+    print('Projetos Divisiveis: \n')
+    for p in self.lpdiva:
+        print p.nome
 
     return contpdiva
 	
 	
 	
-#12 - QUANTIDADE DE PROJETOS DIVISÍVEIS EM CADA ETAPA DO FUNIL
+#12 - QUANTIDADE DE PROJETOS DIVISIVEIS EM CADA ETAPA DO FUNIL
 
 def QprojDive(estado_x):
 
     lpdive = []
     contpdive = 0
-        for p in estado_x.P_e[e]:
-            if( p.vdiv == 1):
-                lpdive.append(p)
-                contpdive = contpdive + 1
+    for p in estado_x.P_e[e]:
+        if( p.vdiv == 1):
+            lpdive.append(p)
+            contpdive = contpdive + 1
 	
-    print('Projetos Divisíveis: \n')
-        for p in self.lpdive:
-            print p.nome
+    print('Projetos Divisiveis: \n')
+    for p in self.lpdive:
+        print p.nome
 
     return contpdive
 	
